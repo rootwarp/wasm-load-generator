99a7d7eaa36a4f35479f6093f2acb76151ea08bc
Joonkyo Kim <rootwarp@gmail.com>
2022-04-18 07:24:07 +0000

    Calculate TPS.

--- main.go
+++ main.go
@@ -2,19 +2,56 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os/exec"
 	"sync"
+	"time"
 )
+
+const (
+	elapseTickPeriod = 100 * time.Millisecond
+)
+
+type statistic struct {
+	Time           time.Time
+	TotalRequest   int64
+	SuccessRequest int64
+	FailRequest    int64
+	Elapse         time.Duration
+}
 
 // TODO: Get N parameter.
 // TODO: Target TPS.
 func main() {
-	fmt.Println("vim-go")
+	sChan := make(chan int, 100)
+	fChan := make(chan int, 100)
+	cmdChan := make(chan string, 100)
+
+	statChan := tpsCalculator(sChan, fChan)
+
+	go addQueue(cmdChan)
+	go printTPS(statChan)
+
+	ctx := context.Background()
+	startWorkers(ctx, 10, cmdChan, sChan, fChan)
 }
 
-func startWorkers(ctx context.Context, n int, c <-chan bool, cmd string) {
+func addQueue(cmdChan chan<- string) {
+	for {
+		_ = <-time.Tick(1 * time.Millisecond)
+		cmdChan <- "./script/test.sh"
+	}
+
+}
+
+func printTPS(statChan <-chan statistic) {
+	for {
+		stat := <-statChan
+		log.Printf("Req %d/%d, TPS: %f\n", stat.SuccessRequest, stat.TotalRequest, float64(stat.SuccessRequest)/stat.Elapse.Seconds())
+	}
+}
+
+func startWorkers(ctx context.Context, n int, cmdChan <-chan string, successChan, failChan chan<- int) {
 	log.Println("Start Worker")
 
 	wg := sync.WaitGroup{}
@@ -28,17 +65,18 @@ func startWorkers(ctx context.Context, n int, c <-chan bool, cmd string) {
 
 			for {
 				select {
-				case r, ok := <-c:
+				case cmd, ok := <-cmdChan:
 					if !ok {
 						return
 					}
 
-					log.Println("Handle", idx)
-					cmd := exec.Command(cmd)
-					err := cmd.Run()
-
-					log.Println(err)
-					_ = r
+					c := exec.Command(cmd)
+					err := c.Run()
+					if err != nil {
+						failChan <- 1
+					} else {
+						successChan <- 1
+					}
 				case <-ctx.Done():
 					return
 				}
@@ -49,6 +87,31 @@ func startWorkers(ctx context.Context, n int, c <-chan bool, cmd string) {
 	wg.Wait()
 }
 
+func tpsCalculator(successChan, failChan <-chan int) chan statistic {
+	tpsChan := make(chan statistic, 1)
+
+	go func() {
+		stat := statistic{}
+
+		for {
+			select {
+			case n := <-successChan:
+				stat.SuccessRequest += int64(n)
+				stat.TotalRequest += int64(n)
+			case n := <-failChan:
+				stat.FailRequest += int64(n)
+				stat.TotalRequest += int64(n)
+			case <-time.Tick(elapseTickPeriod):
+				stat.Time = time.Now()
+				stat.Elapse += elapseTickPeriod
+				tpsChan <- stat
+			}
+		}
+	}()
+
+	return tpsChan
+}
+
 // TODO: Load wasm binary by code.
 // TODO: Call wasm contract. Require heavy load contract.
-// TODO: Show TPS.
+// TODO: Report output.
--- main_test.go
+++ main_test.go
@@ -10,19 +10,34 @@ func TestWorkerCancel(t *testing.T) {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 
-	// TODO: bool -> request
-	c := make(chan bool)
+	c := make(chan string)
 
 	go func() {
 		time.Sleep(time.Second * 1)
 
 		for i := 0; i < 10; i++ {
-			c <- true
+			c <- "./script/test.sh"
 		}
 
-		time.Sleep(time.Second * 1)
 		cancel()
 	}()
 
-	startWorkers(ctx, 5, c, "./script/test.sh")
+	successChan := make(chan int)
+	failChan := make(chan int)
+
+	startWorkers(ctx, 5, c, successChan, failChan)
+}
+
+func TestTPSCalculation(t *testing.T) {
+	sChan := make(chan int, 100)
+	fChan := make(chan int, 100)
+	cmdChan := make(chan string, 100)
+
+	statChan := tpsCalculator(sChan, fChan)
+
+	go addQueue(cmdChan)
+	go printTPS(statChan)
+
+	ctx := context.Background()
+	startWorkers(ctx, 10, cmdChan, sChan, fChan)
 }
